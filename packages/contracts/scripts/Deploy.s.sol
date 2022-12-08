3a1a80c33e467fdfe95f21cfd4a4016e5355ad5e
Federico Luzzi <fedeluzzi00@gmail.com>
2022-12-08 17:55:37 +0000

    feat(contracts): improve deploy script (it now prints the deployment address)

--- packages/contracts/scripts/Deploy.s.sol
+++ packages/contracts/scripts/Deploy.s.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
 import {Redemptor} from "src/Redemptor.sol";
 
 contract Deploy is Script {
-    function run(address[] memory _signers) public {
+    function run(uint256 _signersThreshold, address[] memory _signers) public {
         vm.broadcast();
-        new Redemptor(_signers);
+        Redemptor _redemptor = new Redemptor(_signersThreshold, _signers);
+        console2.log("Redemptor deployed at address", address(_redemptor));
     }
 }
